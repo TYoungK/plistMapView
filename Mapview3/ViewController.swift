70a40127edeb0ec6c67da3314761a2c8fa81747b
D7703_29 <d7703_29@D7703-29ui-iMac.local>
2017-09-21 10:16:38 +0900

    plist

--- Mapview3/ViewController.swift
+++ Mapview3/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,MKMapViewDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "ViewPoint", ofType: "plist")
+        print("path=\(String(describing: path))")
+        let contents = NSArray(contentsOfFile: path!)
+        print("contents=\(String(describing: contents))")
+        
+        var annotations = [MKPointAnnotation]()
+        
+        if let myItem = contents{
+            for item in myItem{
+                let lat = (item as AnyObject).value(forKey: "let")
+                let long = (item as AnyObject).value(forKey: "long")
+                let title = (item as AnyObject).value(forKey: "title")
+                let subtitle = (item as AnyObject).value(forKey: "subtitle")
+                let annotation = MKPointAnnotation()
+                
+                print("lat = \(String(describing: lat))")
+                
+                let myLat = (lat as! NSString).doubleValue
+                let myLong = (long as! NSString).doubleValue
+                
+                print("myLat = \(myLat)")
+                
+                annotation.coordinate.latitude = myLat
+                annotation.coordinate.longitude = myLong
+                annotation.title = title as? String
+                annotation.subtitle = subtitle as? String
+                
+                annotations.append(annotation)
+                map.delegate = self
+            }
+        }
+        map.showAnnotations(annotations, animated: true)
+        map.addAnnotations(annotations)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationtocenter(){
+        
+        let center = CLLocationCoordinate2DMake(36.166197,129.072594)
+        let span = MKCoordinateSpanMake(0.05,0.05)
+        let region = MKCoordinateRegionMake(center, span)
+        map.setRegion(region, animated: true)
     }
-
-
 }
 
